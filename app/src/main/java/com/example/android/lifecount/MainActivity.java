571b758a5d5f2e42c0f60b55ad74d0e0a4c65892
Thorsten <t.boehme1989@gmail.com>
2017-03-23 19:21:45 +0100

    fixed weird rearrange

--- app/src/main/java/com/example/android/lifecount/MainActivity.java
+++ app/src/main/java/com/example/android/lifecount/MainActivity.java
@@ -10,11 +10,49 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    final Button plusHoldRedButton = (Button) findViewById(R.id.plusRHold);
-    boolean plusRedHold = false;
-    int lifeRed;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-{
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
+    boolean plusRedHold = false;
+    class plusRedHoldTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            while (plusRedHold) {
+                lifeRed = lifeRed +1;
+                displayLifeRed(lifeRed);
+                wait(100);
+            }
+            return null;
+        }
+    }
+    
+    final Button plusHoldRedButton = (Button) findViewById(R.id.plusRHold);
+    plusHoldRedButton.setOnTouchListener(new View.OnTouchListener() {
         @Override
         public boolean onTouch(View v, MotionEvent event) {
             switch (event.getAction()) {
@@ -33,63 +71,22 @@ public class MainActivity extends AppCompatActivity {
             }
             return true;
         }
-    }
-    
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-    }
-    plusHoldRedButton.setOnTouchListener(new View.OnTouchListener()
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     });
 
     public void displayLifeRed(int lifeRed) {
         TextView scoreView = (TextView) findViewById(R.id.lifeCounterRed);
         scoreView.setText(String.valueOf(lifeRed));
     }
-
+    int lifeRed;
     public void plusLifeRed(View v) {
         lifeRed = lifeRed + 1;
         displayLifeRed(lifeRed);
     }
 
+    }
     public void minusLifeRed(View v) {
         lifeRed = lifeRed - 1;
         displayLifeRed(lifeRed);
-    }
-
-    }
-
-    class plusRedHoldTask extends AsyncTask<Void, Void, Void> {
-
-        @Override
-        protected Void doInBackground(Void... arg0) {
-            while (plusRedHold) {
-                lifeRed = lifeRed +1;
-                displayLifeRed(lifeRed);
-                wait(100);
-            }
-            return null;
-        }
     }
 
 }
